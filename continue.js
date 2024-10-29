f5432e28aa6652150feea3db7fde6c62e0bf9d43
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-10-29 21:31:08 +0600

    added continue file

---
+++ continue.js
@@ -0,0 +1,17 @@
+// Even number 
+
+for (i = 1; i <= 10; i++) {
+    if (i % 2 === 1) {
+        continue;
+    }
+    // console.log(i);
+}
+
+// odd number 
+
+for (i=1; i<=10; i++){
+    if(i%2 === 0){
+        continue;
+    }
+    console.log(i);
+}
