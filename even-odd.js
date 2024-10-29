267d5a6a1c0928788472cb1e5ab39613689042cd
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-10-29 18:48:42 +0600

    added files

---
+++ even-odd.js
@@ -0,0 +1,10 @@
+// even number 
+for(let i=2; i<=10; i+=2){
+    // console.log(i);
+}
+
+// odd number 
+
+for(let i=1; i <=10; i+=2){
+    console.log(i);
+}
---
+++ while.js
@@ -0,0 +1,11 @@
+// const number = 7;
+// if (number < 10){
+//     console.log("Calaya dao..");
+// }
+// console.log('Na ami calabo na..')
+
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+    num ++;
+}
